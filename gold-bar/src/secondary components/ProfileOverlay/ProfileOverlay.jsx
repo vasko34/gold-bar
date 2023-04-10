6a29017e80b9b75bdd31aa8bb7d8f05d7623dc29
vasko34 <vasko_botsev_34@abv.bg>
2023-04-11 01:12:19 +0300

    fixed navigation in ProfileOverlay

--- gold-bar/src/secondary components/ProfileOverlay/ProfileOverlay.jsx
+++ gold-bar/src/secondary components/ProfileOverlay/ProfileOverlay.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Firebase } from "../../global";
-import { getAuth, signOut  } from 'firebase/auth';
+import { getAuth, signOut, onAuthStateChanged  } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import './profileoverlay.css';
 import { FaTimes } from 'react-icons/fa';
 import { SecurityOverlay } from '../index.js';
@@ -10,7 +12,28 @@ import { SecurityOverlay } from '../index.js';
 const ProfileOverlay = ({ close, library, orders, home }) => {
     const navigate = useNavigate();
     const [toggleSecurityOverlay, setToggleSecurityOverlay] = React.useState(null);
+    const [user, setUser] = React.useState(null);
+    const [adminStatus, setAdminStatus] = React.useState(null);
     const auth = getAuth(Firebase);
+    const db = getFirestore(Firebase);
+
+    React.useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+        });
+
+        return unsubscribe;
+    }, [auth]);
+  
+    React.useEffect(() => {
+        const getAdminStatus = async () => {
+            if (!user) return;
+            const userDoc = await getDoc(doc(db, "users", user.uid));
+            const adminStatus = userDoc.data().adminStatus;
+            setAdminStatus(adminStatus);
+        };
+        getAdminStatus();
+    }, [user]);
 
     const openSecurityOverlay = () => {
         setToggleSecurityOverlay(true);
@@ -31,7 +54,7 @@ const ProfileOverlay = ({ close, library, orders, home }) => {
                 <h2>Profile</h2>
                 { !library && (<p onClick = { () => navigate('/library') }>Library</p>) }
                 { !orders && (<p onClick = { () => navigate('/orders') }>Orders</p>) }
-                { !home && (<p onClick = { () => navigate('/user') }>Home</p>) }
+                { !home && (adminStatus ? (<p onClick = { () => navigate('/admin') }>Home</p>) : (<p onClick = { () => navigate('/user') }>Home</p>)) }
                 <button type = 'button' onClick = { openSecurityOverlay }>Log out</button>
                 <FaTimes onClick = { close } className = 'close'></FaTimes>
             </div>
