40834c9f912b5869e2c3a2276d9c5139a8ad8804
vasko34 <vasko_botsev_34@abv.bg>
2023-06-16 19:45:52 +0300

    First Official Finish of Gold Bar

--- gold-bar/src/secondary components/ProfileOverlay/ProfileOverlay.jsx
+++ gold-bar/src/secondary components/ProfileOverlay/ProfileOverlay.jsx
@@ -55,7 +55,7 @@ const ProfileOverlay = ({ close, library, orders, home }) => {
                 { !home && (adminStatus ? (<p onClick = { () => navigate('/admin') }>Home</p>) : (<p onClick = { () => navigate('/user') }>Home</p>)) }
                 { (adminStatus ? (<button type = 'button' onClick = { handleLogOut }>Log out</button>) : (<button type = 'button' onClick = { openSecurityOverlay }>Log out</button>)) }              
                 <FaTimes onClick = { close } className = 'close'></FaTimes>
-            </div>
+            </div>            
             { toggleSecurityOverlay && (<SecurityOverlay close = { closeSecurityOverlay } closeProfileOverlay = { close } logoutUser = { handleLogOut }></SecurityOverlay>) }
         </div>
     );
